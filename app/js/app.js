0d04661e20eaad99d6e98a5a01fc8342f8abea93
Alexander Pletnev <pletnev.a.a@yandex.ru>
2021-10-05 17:06:48 +0300

    Сделал шапку и хедер для главной

--- app/js/app.js
+++ app/js/app.js
@@ -2,12 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const swiper = new Swiper('.welcome-slider', {
 		// Optional parameters
-		autoHeight: "auto",
+		/* autoHeight: "auto", */
 		loop: true,
 	  
 		// If we need pagination
 		pagination: {
-		  el: '.swiper-pagination',
+            el: '.swiper-pagination',
+            clickable: 'true',
 		},
 	  
 		// Navigation arrows
@@ -17,5 +18,36 @@ document.addEventListener('DOMContentLoaded', () => {
 		},
 
 	});
+
+	const menu = document.querySelector('.mobile-menu'),
+        burger = document.querySelector('.burger'),
+        overlay = document.querySelector('.overlay'),
+        closeMenu = document.querySelector('.mobile-menu__close');
+
+    const lockScroll = () => {
+        document.body.classList.add('lock');
+    }
+
+    const unlockScroll = () => {
+        document.body.classList.remove('lock');
+    }
+
+    burger.addEventListener('click', () => {
+        menu.classList.add('open');
+        overlay.classList.add('open');
+        lockScroll();
+    });
+
+    closeMenu.addEventListener('click', () => {
+        menu.classList.remove('open');
+        overlay.classList.remove('open');
+        unlockScroll();
+    });
+
+    overlay.addEventListener('click', () => {
+        menu.classList.remove('open');
+        overlay.classList.remove('open');
+        unlockScroll();
+    });
 
 })
